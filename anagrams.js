2fbc9d918a991553012c223ff7957ab56d941b28
Vinayak Santhosh <18r01a04b3@cmritonline.ac.in>
2025-02-27 22:22:06 +0530

    javascript-practice-problems-27-02-2025

--- anagrams.js
+++ anagrams.js
@@ -1,17 +1,26 @@
 let stringA = "Hi there", stringB = "Bye there";
 let mapA = {}, mapB = {};
-stringA.split(" ").forEach((word) => {
-    word.split("").map((char) => {
+//Solution 1
+stringA.toLowerCase().split(" ").forEach((word) => {
+    word.split("").filter(char=>char >= 'a' && char <= 'z').map((char) => {
         mapA[char] = (mapA[char] || 0) + 1;
     })
 })
-stringB.split(" ").forEach((word) => {
-    word.split("").map((char) => {
+stringB.toLowerCase().split(" ").forEach((word) => {
+    word.split("").filter(char=>char >= 'a' && char <= 'z').map((char) => {
         mapB[char] = (mapB[char] || 0) + 1;
     })
 })
-
+let arrA = stringA.toLowerCase().split(" ").splice(1);
+console.log(arrA)
+let anagram = true;
+for (const char in mapA) {
+    if(!stringB.includes(char)) {
+        anagram = false;
+        break;
+    }
+}
 console.log(mapA)
 console.log(mapB)
-
+console.log(anagram)
 // console.log(mapA.includes(mapB))
